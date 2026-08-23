072696a8bb397c93601e03ddcd2d7790c0d81b6d
PRINCE369art <princekumar42861@gmail.com>
2026-08-23 17:56:01 +0530

    Add Mad Libs Generator

---
+++ Mad_Libs_Generator.py
@@ -0,0 +1,6 @@
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjectivve: ")
+
+story=(f"Today i saw a {noun}. It was very {adjective}. suddenly it started to {verb}")
+print(story)
