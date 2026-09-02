9183e4ab51e8e43d475cd5d05b3e877ee0ec9562
PRINCE369art <princekumar42861@gmail.com>
2026-09-02 11:09:19 +0530

    making gitigonore file and modified the to_do_list

--- To-Do List.py
+++ To-Do List.py
@@ -1,22 +1,22 @@
-tasks = []
+tasks = []  # this make the list empty and ready to store tasks
 
 print("===== TO-DO LIST =====")
 
 # Add tasks
-for i in range(5):
+for i in range(5):                   # here the for loop is used to add 5 tasks to the list 
     task = input("Enter a task: ")
-    tasks.append(task)
+    tasks.append(task)          # append() method is used to add the task to the list
 
 print("\nYour To-Do List:")
 
 # Display tasks
-for i, task in enumerate(tasks, start=1):
-    print(f"{i}. {task}")
+for i, task in enumerate(tasks, start=1):  #enumerate() function is used to get the index and value of each task in the list, starting from 1
+    print(f"{i}. {task}")  # print each task with its index
 
 # Remove a task
-task_to_remove = input("\nEnter the task you want to remove: ")
+task_to_remove = input("\nEnter the task you want to remove: ") #making tast_to_remove variable to store the task that user wants to remove from the list
 
-if task_to_remove in tasks:
+if task_to_remove in tasks:  # in operator is used to check if the task is present in the list or not
     tasks.remove(task_to_remove)
     print("Task removed successfully!")
 else:
